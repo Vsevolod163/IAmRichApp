240ae1b0d1680a133e4331b4c60c676a5dfe4dbc
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-28 23:20:15 +0300

    Initial Commit

---
+++ IAmRichApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRichApp
+//
+//  Created by Vsevolod Lashin on 28.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
